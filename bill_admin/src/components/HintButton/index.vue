49431c402e1760317828ebcf810e1fbddb3c61aa
M1748595804 <1748595804@qq.com>
2022-03-22 00:38:14 +0800

    后台管理—账单管理-前端

--- bill_admin/src/components/HintButton/index.vue
+++ bill_admin/src/components/HintButton/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <a href="javascript:;" :title="title">
+  <el-tooltip class="item" effect="dark" :content="title" placement="bottom">
     <el-button v-bind="$attrs" v-on="$listeners"></el-button>
-  </a>
+  </el-tooltip>
 </template>
 
 <script>
 export default {
   name: 'HintButton',
-  props:['title']
+  props: ['title']
 }
 </script>
 
 <style lang="scss" scoped>
-  a{
-    margin-right:10px
-  }
+a {
+  margin-right: 10px;
+}
 </style>
