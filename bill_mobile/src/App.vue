432e866326e13db4adefe2156fb83f365d534868
M1748595804 <1748595804@qq.com>
2022-03-23 00:27:27 +0800

    H5开发

--- bill_mobile/src/App.vue
+++ bill_mobile/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="more-o">详细</van-tabbar-item>
+      <van-tabbar-item icon="chart-trending-o">图表</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.van-tabbar {
+  border-top: 1px solid #d9d9d9;
 }
 </style>
--- bill_mobile/src/views/Home.vue
+++ bill_mobile/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-nav-bar title="首页">
+      <template #right>
+        <van-icon name="user-o" size="22" />
+      </template>
+      <template #left>
+        <img src="../assets/img/logo.png" style="height:46px;width:81px" alt="">
+      </template>
+    </van-nav-bar>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
 </script>
+<style lang="less">
+.van-tabbar {
+  border-top: 1px solid #d9d9d9;
+}
+</style>
