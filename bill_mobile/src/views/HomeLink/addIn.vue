3574079c998a1476ab5ac2782b4283514ae8f4c7
M1748595804 <1748595804@qq.com>
2022-04-05 23:58:32 +0800

    添加消费和添加收入的表单

--- bill_mobile/src/views/HomeLink/addIn.vue
+++ bill_mobile/src/views/HomeLink/addIn.vue
@@ -1,16 +1,109 @@
 <template>
   <div>
-    <Header :leftIcon="'arrow-left'" :title="'收入记录'"></Header>
+    <Header :leftIcon="'arrow-left'" :title="'添加收入'"></Header>
     <div class="layout">
-      收入
+      <van-tabs @click="onClick">
+        <van-tab title="工资收入">
+          <van-form validate-first @failed="onFailed">
+            <van-field v-model="phone" required label="日期" @click="showPopup" placeholder="请选择日期" />
+            <van-popup v-model="show" position="bottom" :style="{ height: '40%' }">
+              <van-datetime-picker v-model="currentDate" type="year-month" title="选择年月" :min-date="minDate" :max-date="maxDate" :formatter="formatter" />
+            </van-popup>
+            <van-field v-model="phone" label="工资收入" placeholder="请输入金额" />
+            <van-field v-model="phone" label="社保扣除" placeholder="请输入金额" />
+            <van-field v-model="phone" label="公积金" placeholder="请输入金额" />
+            <van-field v-model="phone" label="其他扣除" placeholder="请输入金额" />
+            <van-field v-model="phone" label="扣除后工资" placeholder="请输入金额" />
+            <div style="margin: 16px;">
+              <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+          </van-form>
+        </van-tab>
+        <van-tab title="其他收入">
+          <van-form validate-first @failed="onFailed">
+            <van-field v-model="phone" required label="日期" @click="showPopup" placeholder="请选择日期" />
+            <van-popup v-model="show" position="bottom" :style="{ height: '40%' }">
+              <van-datetime-picker v-model="currentDate" type="date" title="选择年月日" :min-date="minDate" :max-date="maxDate" />
+            </van-popup>
+            <van-cell is-link title="选择分类" @click="showType = true" />
+            <van-action-sheet v-model="showType" :actions="actions" style='height:40%' @select="onSelect" />
+            <van-field v-model="phone" label="姓名" placeholder="请输入姓名" />
+            <van-field v-model="phone" label="重量" placeholder="请输入重量" />
+            <van-field v-model="phone" label="价格" placeholder="请输入价格" />
+            <van-field v-model="phone" label="邮费" placeholder="请输入邮费" />
+            <van-row>
+              <van-col span="8">是否包邮</van-col>
+              <van-col span="16">
+                <van-radio-group v-model="radio" direction="horizontal">
+                  <van-radio name="1">单选框 1</van-radio>
+                  <van-radio name="2">单选框 2</van-radio>
+                </van-radio-group>
+              </van-col>
+            </van-row>
+
+            <van-field v-model="phone" label="合计" placeholder="请输入合计" />
+            <van-field v-model="phone" label="收款" placeholder="请输入收款" />
+            <div style="margin: 16px;">
+              <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+          </van-form>
+        </van-tab>
+      </van-tabs>
+
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
-}
+  name: 'AddIn',
+  data() {
+    return {
+      value1: '',
+      value2: '',
+      value3: '',
+      pattern: /\d{6}/,
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(2021, 0, 17),
+      phone: '',
+      show: false,
+      showType: false,
+      actions: [{ name: '葛粉收入' }, { name: '生活买菜' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }],
+      radio: '1',
+    };
+  },
+  methods: {
+    formatter(type, val) {
+      if (type === 'year') {
+        return `${val}年`;
+      } else if (type === 'month') {
+        return `${val}月`;
+      }
+      return val;
+    },
+    // 校验函数返回 true 表示校验通过，false 表示不通过
+    validator(val) {
+      return /1\d{10}/.test(val);
+    },
+    // 异步校验函数返回 Promise
+    onFailed(errorInfo) {
+      console.log('failed', errorInfo);
+    },
+    showPopup() {
+      this.show = true;
+    },
+    onSelect(item) {
+      // 默认情况下点击选项时不会自动收起
+      // 可以通过 close-on-click-action 属性开启自动收起
+      this.show = false;
+      this.$toast(item.name);
+    },
+    onClick(name, title) {
+      this.$toast(title);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
--- bill_mobile/src/views/HomeLink/addpay.vue
+++ bill_mobile/src/views/HomeLink/addpay.vue
@@ -2,29 +2,64 @@
   <div>
     <Header :leftIcon="'arrow-left'" :title="'添加账单'"></Header>
     <div class="layout">
-      <van-form validate-first @failed="onFailed">
-        <!-- 通过 pattern 进行正则校验 -->
-        <van-field v-model="value1" name="pattern" placeholder="正则校验" :rules="[{ pattern, message: '请输入正确内容' }]" />
-        <!-- 通过 validator 进行函数校验 -->
-        <van-field v-model="value2" name="validator" placeholder="函数校验" :rules="[{ validator, message: '请输入正确内容' }]" />
-        <!-- 通过 validator 进行异步函数校验 -->
-        <van-field v-model="value3" name="asyncValidator" placeholder="异步函数校验" :rules="[{ validator: asyncValidator, message: '请输入正确内容' }]" />
-        <div style="margin: 16px;">
-          <van-button round block type="info" native-type="submit">提交</van-button>
-        </div>
-      </van-form>
+      <van-tabs @click="onClick">
+        <van-tab title="单一记录">
+          <van-form validate-first @failed="onFailed">
+            <van-field v-model="phone" required label="日期" @click="showPopup" placeholder="请选择日期" />
+            <van-popup v-model="show" position="bottom" :style="{ height: '40%' }">
+              <van-datetime-picker v-model="currentDate" type="date" title="选择年月日" :min-date="minDate" :max-date="maxDate" />
+            </van-popup>
+            <van-cell is-link title="选择分类" @click="showType = true" />
+            <van-action-sheet v-model="showType" :actions="actions" style='height:40%' @select="onSelect" />
+            <van-field v-model="phone" label="金额" placeholder="请输入金额" />
+            <div style="margin: 16px;">
+              <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+          </van-form>
+        </van-tab>
+        <van-tab title="批量记录">
+          <van-form validate-first @failed="onFailed">
+            <van-field v-model="phone" required label="日期" @click="showPopup" placeholder="请选择日期" />
+            <van-popup v-model="show" position="bottom" :style="{ height: '40%' }">
+              <van-datetime-picker v-model="currentDate" type="date" title="选择年月日" :min-date="minDate" :max-date="maxDate" />
+            </van-popup>
+            <van-field v-model="phone" label="快餐饮食" placeholder="请输入金额" />
+            <van-field v-model="phone" label="生活买菜" placeholder="请输入金额" />
+            <van-field v-model="phone" label="交通出行" placeholder="请输入金额" />
+            <van-field v-model="phone" label="日用百货" placeholder="请输入金额" />
+            <van-field v-model="phone" label="话费充值" placeholder="请输入金额" />
+            <van-field v-model="phone" label="车辆保养" placeholder="请输入金额" />
+            <van-field v-model="phone" label="服装服饰" placeholder="请输入金额" />
+            <van-field v-model="phone" label="水果零食" placeholder="请输入金额" />
+            <van-field v-model="phone" label="医药健康" placeholder="请输入金额" />
+            <van-field v-model="phone" label="其他" placeholder="请输入金额" />
+            <div style="margin: 16px;">
+              <van-button round block type="info" native-type="submit">提交</van-button>
+            </div>
+          </van-form>
+        </van-tab>
+      </van-tabs>
+
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'AddPay',
   data() {
     return {
       value1: '',
       value2: '',
       value3: '',
       pattern: /\d{6}/,
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(2021, 0, 17),
+      phone: '',
+      show: false,
+      showType: false,
+      actions: [{ name: '快餐饮食' }, { name: '生活买菜' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }, { name: '交通出行' }],
     };
   },
   methods: {
@@ -33,18 +68,20 @@ export default {
       return /1\d{10}/.test(val);
     },
     // 异步校验函数返回 Promise
-    asyncValidator(val) {
-      return new Promise((resolve) => {
-        Toast.loading('验证中...');
-
-        setTimeout(() => {
-          Toast.clear();
-          resolve(/\d{6}/.test(val));
-        }, 1000);
-      });
-    },
     onFailed(errorInfo) {
       console.log('failed', errorInfo);
+    },
+    showPopup() {
+      this.show = true;
+    },
+    onSelect(item) {
+      // 默认情况下点击选项时不会自动收起
+      // 可以通过 close-on-click-action 属性开启自动收起
+      this.show = false;
+      this.$toast(item.name);
+    },
+    onClick(name, title) {
+      this.$toast(title);
     },
   },
 };
