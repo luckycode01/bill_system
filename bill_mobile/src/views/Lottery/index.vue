26921bed7b157a7cde0f0980e5753b952988a6e6
M1748595804 <1748595804@qq.com>
2022-04-14 00:34:24 +0800

    随机彩票

--- bill_mobile/src/views/Lottery/index.vue
+++ bill_mobile/src/views/Lottery/index.vue
@@ -4,18 +4,21 @@
     <div class="layout">
       <van-tabs v-model="activeName" @change="changeActiveTabs" color="#1989fa">
         <van-tab name="1" title="大乐透">
+          <div class="title">中奖号码</div>
+          <div class="win">
+            <div class="isWin">1</div>
+          </div>
           <div class="title">已选列表</div>
           <div class="result_list" v-for="(item,index) in resultList" :key="index">
-            <div class="list_item" v-for="(v,i) in item.frontArea" :key="i">{{v}}</div>
-            <div class="list_item blue" v-for="(v,i) in item.backArea" :key="i">{{v}}</div>
+            <div class="isWin">{{index}}</div>
+            <div class="list_item" v-for="(v,i) in item.frontArea" :key="i+'key'">{{v}}</div>
+            <div class="list_item blue" v-for="(v,i) in item.backArea" :key="i+'key1'">{{v}}</div>
+          </div>
+          <div style="width:355px;margin: auto;display:flex;  justify-content: space-around;">
+            <van-button @click="handleRandom" type="info" style="width:45%" size="large">随机一注</van-button>
+            <van-button @click="handleRandom" type="info" style="width:45%" size="large">固定号码</van-button>
           </div>
         </van-tab>
-        <div style="width:355px;margin: auto;">
-          <van-button @click="handleRandom" type="info" size="large">随机一注</van-button>
-        </div>
-        <div style="width:355px;margin:20px auto;padding-bottom: 20px;">
-          <van-button @click="handleRandom" type="info" size="large">固定号码</van-button>
-        </div>
 
         <van-tab name="2" title="双色球">
           <div>双色球</div>
@@ -31,12 +34,8 @@ export default {
   name: 'Lottery',
   data() {
     return {
-      resultList: [
-        {
-          frontArea: [1, 1, 2, 22, 23],
-          backArea: [3, 4],
-        }
-      ]
+      activeName: '1',
+      resultList: []
 
     }
   },
@@ -44,10 +43,42 @@ export default {
   },
   methods: {
     handleRandom() {
+      var redBall = [];
+      var greenBall = [];
+      var flag;
+      while (redBall.length < 5) {
+        let flag = true;
+        let creatRed = Math.floor(Math.random() * 33) + 1;
+        for (var i = 0; i < redBall.length; i++) {
+          if (creatRed == redBall[i]) {
+            flag = false;
+          }
+        }
+        if (flag) {
+          redBall[redBall.length] = creatRed;
+        }
+      }
+      while (greenBall.length < 2) {
+        let flag = true;
+        let creatGreen = Math.floor(Math.random() * 12) + 1;
+        for (var i = 0; i < greenBall.length; i++) {
+          if (creatGreen == greenBall[i]) {
+            flag = false;
+          }
+        }
+        if (flag) {
+          greenBall[greenBall.length] = creatGreen;
+        }
+      }
       this.resultList.push({
-        frontArea: [1, 1, 2, 22, 23],
-        backArea: [3, 4],
+        frontArea: redBall,
+        backArea: greenBall,
+
       })
+
+    },
+    changeActiveTabs() {
+
     }
   },
 };
@@ -65,23 +96,45 @@ export default {
 .result_list {
   width: 355px;
   margin: 10px auto;
-  padding: 0px 0px 11px 0px;
+  padding: 10px 0;
   box-sizing: border-box;
   display: flex;
   justify-content: space-around;
   border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  align-items: center;
+  background: e1dede;
   .list_item {
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
     background: red;
     border-radius: 50%;
+    background: radial-gradient(circle at 70% 30%, #ff8585, #ff0000);
     text-align: center;
-    line-height: 40px;
+    line-height: 30px;
     color: #ffffff;
-    font-size: 18px;
+    font-size: 16px;
   }
   .blue {
-    background-color: #4725f1;
+    background: radial-gradient(circle at 70% 30%, #8d85e6, #0400ff);
+  }
+}
+.isWin {
+  width: 15px;
+  height: 15px;
+  border: 1px solid #ee0000;
+  color: #ee0000;
+  border-radius: 50%;
+  line-height: 15px;
+  display: flex;
+  justify-content: center;
+}
+.win {
+  display: flex;
+  width: 355px;
+  margin: auto;
+  div {
+    margin-right: 10px;
+    margin-bottom: 10px;
   }
 }
 </style>
