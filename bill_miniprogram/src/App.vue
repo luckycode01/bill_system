201bfcb46b9df0ff5795cb339413684cbc07d176
13451167279 <1748595804@qq.com>
2022-11-09 00:08:16 +0800

    first submit

--- bill_miniprogram/src/App.vue
+++ bill_miniprogram/src/App.vue
@@ -13,21 +13,5 @@
 </script>
 
 <style>
-	@import './common/common.css';
 
-	page,
-	view {
-		display: flex;
-	}
-
-	page {
-		display: flex;
-		min-height: 100%;
-		background-color: #EFEFEF;
-	}
-
-	template {
-		display: flex;
-		flex: 1;
-	}
 </style>
