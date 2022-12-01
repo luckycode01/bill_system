54578efbb661c574b16ebd66f7792c74c893c63d
13451167279 <1748595804@qq.com>
2022-12-01 23:29:17 +0800

    初始化项目

---
+++ bill_miniprogram/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="header">
+		<view class="back">
+			<text class=""></text>
+		</view>
+		<view class="title"></view>
+		<view class="right"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.header{
+		width: 100%;
+	}
+</style>
