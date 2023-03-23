cf8e7045c2db1ced43aff8a000c89e96f89a845d
13451167279 <1748595804@qq.com>
2023-03-24 00:31:40 +0800

    新增消费表单

--- bill_miniprogram/src/pages/billManage/addpay.vue
+++ bill_miniprogram/src/pages/billManage/addpay.vue
@@ -11,7 +11,7 @@
     <view class="form_main">
       <u-form
         labelPosition="left"
-        :model="dataForm"
+        :model="addOneForm"
         :rules="dataFormRules"
         ref="dataFormRef"
       >
@@ -40,7 +40,7 @@
           ref="item1"
         >
           <u-input
-            v-model="dataForm.dateTime"
+            v-model="dataForm.categoryValue"
             placeholder="请选择分类"
             border="none"
             disabled
@@ -49,22 +49,166 @@
         </u-form-item>
         <u-form-item label="金额" prop="amount" required borderBottom>
           <u-input
+            v-model="dataForm.money"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+      </u-form>
+      <u-form
+        labelPosition="left"
+        labelWidth="80"
+        :model="batchFormData"
+        :rules="dataFormRules"
+        ref="batchFormRef"
+      >
+        <u-form-item
+          label="日期"
+          prop="dateTime"
+          borderBottom
+          required
+          @click="hideOpenPicker('time')"
+        >
+          <u-input
+            v-model="dataForm.dateTime"
+            placeholder="请选择时间"
+            border="none"
+            disabled
+            disabledColor="#fff"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="快餐饮食" prop="amount" required borderBottom>
+          <u-input
             v-model="dataForm.amount"
             placeholder="请输入金额"
             border="none"
           ></u-input>
         </u-form-item>
       </u-form>
+      <u-form
+        labelPosition="left"
+        labelWidth="80"
+        :model="houseFormData"
+        :rules="dataFormRules"
+        ref="houseFormRef"
+      >
+        <u-form-item
+          label="日期"
+          prop="dateTime"
+          borderBottom
+          required
+          @click="hideOpenPicker('time')"
+          ref="item1"
+        >
+          <u-input
+            v-model="dataForm.dateTime"
+            placeholder="请选择时间"
+            border="none"
+            disabled
+            disabledColor="#fff"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="房租" prop="houseMoney" required borderBottom>
+          <u-input
+            v-model="dataForm.houseMoney"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="水表起码" prop="waterStart" required borderBottom>
+          <u-input
+            v-model="dataForm.waterStart"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="水表止码" prop="waterEnd" required borderBottom>
+          <u-input
+            v-model="dataForm.waterEnd"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="水费单价" prop="waterPrice" required borderBottom>
+          <u-input
+            v-model="dataForm.waterPrice"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item
+          label="电表起码"
+          prop="electricityStart"
+          required
+          borderBottom
+        >
+          <u-input
+            v-model="dataForm.electricityStart"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item
+          label="电表止码"
+          prop="electricityEnd"
+          required
+          borderBottom
+        >
+          <u-input
+            v-model="dataForm.electricityEnd"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item
+          label="电费单价"
+          prop="electricityPrice"
+          required
+          borderBottom
+        >
+          <u-input
+            v-model="dataForm.electricityPrice"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+        <u-form-item label="网费" prop="networkMoney" required borderBottom>
+          <u-input
+            v-model="dataForm.networkMoney"
+            placeholder="请输入金额"
+            border="none"
+          ></u-input>
+        </u-form-item>
+      </u-form>
+    </view>
+    <view class="save_btn">
+      <u-button
+        type="primary"
+        :disabled="loading"
+        :loading="loading"
+        :loadingText="loadingText"
+      >
+        {{ loadingText }}
+      </u-button>
     </view>
     <u-datetime-picker
       mode="date"
-      v-model="dataForm.dateTime"
       :show="showDateTime"
+      v-model="selectTime"
       :closeOnClickOverlay="true"
       @close="showDateTime = false"
       @cancel="showDateTime = false"
       @confirm="handleConfirmTime($event, item)"
     ></u-datetime-picker>
+    <u-picker
+      :show="showCategory"
+      :keyName="'label'"
+      :columns="categoryList"
+      :closeOnClickOverlay="true"
+      @close="showCategory = false"
+      @cancel="showCategory = false"
+      @confirm="handleConfirmCate($event, item)"
+    ></u-picker>
   </view>
 </template>
 
@@ -85,12 +229,53 @@ export default {
           name: "租房记录",
         },
       ],
-      dataForm: {
+      selectTime: Number(new Date()),
+      addOneForm: {
         dateTime: "",
-        amount: "",
+        categoryValue: "",
+        money: "",
       },
+      batchFormData: {
+        selectDate: "",
+        food: "",
+        life: "",
+        traffic: "",
+        dailyuse: "",
+        telephonebill: "",
+        car: "",
+        clothing: "",
+        fruitsnacks: "",
+        health: "",
+        other: "",
+      },
+      houseFormData: {
+        selectDate: "",
+        houseMoney: "",
+        waterStart: "",
+        waterEnd: "",
+        waterPrice: "",
+        electricityStart: "",
+        electricityEnd: "",
+        electricityPrice: "",
+        networkMoney: "",
+        totalMoney: "",
+      },
+      loading: false,
+      loadingText: "保存",
       showDateTime: false,
       showCategory: false,
+      categoryList: [
+        [
+          {
+            label: "雪月夜",
+            id: 2021,
+          },
+          {
+            label: "冷夜雨",
+            id: 804,
+          },
+        ],
+      ],
       dataFormRules: {
         dateTime: {
           required: true,
@@ -147,5 +332,9 @@ export default {
 <style lang="scss" scoped>
 .form_main {
   padding: 48rpx;
+}
+.save_btn {
+  margin: 18rpx auto 36rpx;
+  width: 240rpx;
 }
 </style>
