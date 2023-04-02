bb71c225c443b5b182b3f618d77819837712417f
13451167279 <1748595804@qq.com>
2023-04-02 23:28:56 +0800

    添加悬浮框

---
+++ bill_miniprogram/src/pages/billManage/components/filterBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="popup_main" :style="{paddingTop:headerHeight + 'px'}">
+
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showPopup: false,
+    }
+  },
+  computed: {
+    headerHeight() {
+      return 12 + (getApp().globalData.menuBtn.height || 0) + (getApp().globalData.menuBtn.top || 0);
+    },
+  },
+  created() {
+  },
+  methods: {
+    init() {
+      console.log('init');
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.popup_main {
+  width: 100%;
+  padding: 24rpx;
+}
+</style>
--- bill_miniprogram/src/pages/billManage/plot.vue
+++ bill_miniprogram/src/pages/billManage/plot.vue
@@ -19,18 +19,27 @@
         </u-grid-item>
       </u-grid>
     </view>
-    <view class="echart_box">
+    <view v-show="!showPopup" class="echart_box">
       <EchartBox :initData="payData" :config="payConfig" @handleChangeEchart="handleChangeEchart"></EchartBox>
       <EchartBox :initData="payData" :config="houseConfig" @handleChangeEchart="handleChangeEchart"></EchartBox>
       <EchartBox :initData="payData" :config="livingConfig" @handleChangeEchart="handleChangeEchart"></EchartBox>
     </view>
+    <!-- 悬浮框 -->
+    <view class="filter" :style="{top:moveObj.positionY+'px'}" @click="handleFilterClick" @touchstart="handleTouchStart" @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd">
+    </view>
+    <u-popup :show="showPopup" mode="right" :customStyle="{width:'600rpx'}" @close="handleClosePopup">
+      <filterBox ref="filterBoxRef"></filterBox>
+    </u-popup>
   </view>
 </template>
 <script>
-import EchartBox from "@/components/echarts/index";
+import EchartBox from "@/components/echarts/index.vue";
+import filterBox from "./components/filterBox.vue"
 export default {
   components: {
     EchartBox,
+    filterBox,
   },
   data() {
     return {
@@ -67,26 +76,76 @@ export default {
         graphic: true,
       },
       livingConfig: {
-        type: "Bar",
+        type: "line",
         limitNumber: 5,
         isChange: true,
         configName: 'livingConfig',
         title: "日期标题",
         graphic: true,
       },
+      showPopup: false,
+      clientHeight: 0,
+      moveObj: {
+        positionY: 300,
+        touch: true,
+        tranformY: 0,
+      }
     };
   },
   components: {},
-  onLoad() { },
+  onLoad() {
+    // 获取设备高度
+    let _this = this
+    uni.getSystemInfo({
+      success: function (res) {
+        _this.clientHeight = res.windowHeight;
+      }
+    });
+  },
   methods: {
+    handleFilterClick() {
+      this.showPopup = true;
+    },
     handleChangeEchart(target, val) {
       this.$set(this[target], 'type', val)
-    }
+    },
+    handleTouchStart(e) {
+      this.moveObj.touch = true;
+      this.moveObj.tranformY = e.touches[0].pageY - this.moveObj.positionY;
+    },
+    handleTouchMove(e) {
+      if (!this.moveObj.touch) return;
+      var new_posY = e.touches[0].pageY - this.moveObj.tranformY;
+      if (new_posY < 118) {
+        new_posY = 100;
+      }
+      if (new_posY > this.clientHeight - 100) {
+        new_posY = this.clientHeight - 100;
+      }
+      this.moveObj.positionY = new_posY;
+    },
+    handleTouchEnd(e) {
+      if (!this.moveObj.touch) return;
+      this.moveObj.touch = false;
+      this.moveObj.tranformY = 0;
+    },
+    handleClosePopup() {
+      this.showPopup = false
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
 .plot_mail {
+  position: relative;
+  .filter {
+    position: fixed;
+    width: 80rpx;
+    height: 60rpx;
+    background: #57aaff;
+    right: 0;
+    border-radius: 60rpx 0 0 60rpx;
+  }
   .plot_header {
     padding: 0 24rpx;
     margin: 0 auto 16rpx auto;
