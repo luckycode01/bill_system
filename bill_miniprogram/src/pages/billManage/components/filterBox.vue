ad2863312cbbe47dcef1bdac31117c799e430e3e
maws1008 <1748595804@qq.com>
2023-04-10 23:55:22 +0800

    20230410

--- bill_miniprogram/src/pages/billManage/components/filterBox.vue
+++ bill_miniprogram/src/pages/billManage/components/filterBox.vue
@@ -2,25 +2,26 @@
   <view class="popup_main" :style="{paddingTop:headerHeight + 'px'}">
     <view>
       <el-form>
-        <u-form-item label="日期" prop="dateTime" borderBottom @click="hideOpenPicker('time')" ref="item1">
-          <u-input v-model="addOneForm.dateTime" placeholder="请选择时间" border="none" disabled disabledColor="#fff"></u-input>
-          <u-tag text="标签" plain shape="circle"></u-tag>
-          <u-tag text="标签" plain shape="circle"></u-tag>
+        <u-form-item label="日期" prop="dateTime" borderBottom ref="item1">
+          <view class="form_item">
+            <u-input v-model="addOneForm.dateTime" @click="hideOpenPicker('time')" placeholder="请选择时间" border="none" disabled disabledColor="#fff"></u-input>
+            <view class="time_tag">本月</view>
+            <view class="time_tag ml">本周</view>
+          </view>
         </u-form-item>
       </el-form>
     </view>
     <view class="footer">
       <u-row>
-        <u-col span="5">
+        <u-col :span="5">
           <u-button type="info" :plain="true" text="重置"></u-button>
         </u-col>
-        <u-col span="5" :offset="1">
+        <u-col :span="5" :offset="1">
           <u-button type="primary" text="确定"></u-button>
         </u-col>
       </u-row>
     </view>
-    <u-datetime-picker mode="date" :show="showDateTime" v-model="selectTime" :closeOnClickOverlay="true" @close="showDateTime = false"
-      @cancel="showDateTime = false" @confirm="handleConfirmTime($event, item)">
+    <u-datetime-picker mode="date" :show="showDateTime" v-model="selectTime" :closeOnClickOverlay="true" @close="showDateTime = false" @cancel="showDateTime = false" @confirm="handleConfirmTime($event, item)">
       aa
     </u-datetime-picker>
   </view>
@@ -72,6 +73,24 @@ export default {
     bottom: 0px;
     padding-bottom: constant(safe-area-inset-bottom);
     padding-bottom: env(safe-area-inset-bottom);
+  }
+}
+.form_item {
+  display: flex;
+  justify-content: space-between;
+  .U-input {
+    flex: 1;
+  }
+  .time_tag {
+    width: 88rpx;
+    text-align: center;
+    border: 1px solid #56aaff;
+    border-radius: 40rpx;
+    font-size: 24rpx;
+    height: 40rpx;
+  }
+  .ml {
+    margin-left: 8rpx;
   }
 }
 </style>
