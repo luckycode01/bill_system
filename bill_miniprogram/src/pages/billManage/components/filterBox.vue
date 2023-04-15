b2945ce3b2e08892ec14702e4ca585b2edc80870
13451167279 <1748595804@qq.com>
2023-04-15 17:42:48 +0800

    保存代码

--- bill_miniprogram/src/pages/billManage/components/filterBox.vue
+++ bill_miniprogram/src/pages/billManage/components/filterBox.vue
@@ -1,15 +1,33 @@
 <template>
   <view class="popup_main" :style="{paddingTop:headerHeight + 'px'}">
-    <view>
-      <u-form :model="filterForm" :rules="filterFormRules" ref="filterFormRef">
-        <u-form-item label="日期" prop="dateTime" borderBottom ref="item1">
-          <view class="form_item">
-            <u-input v-model="filterForm.dateTime" @focus="hideOpenPicker('time')" placeholder="请选择时间" border="none" ></u-input>
-            <view class="time_tag">本月</view>
-            <view class="time_tag ml">本周</view>
-          </view>
-        </u-form-item>
-      </u-form>
+    <view class="search">
+      <view class="search_item">
+        <view class="title">
+          <text class="lable">起止时间</text>
+          <view class="time_tag">本月</view>
+          <view class="time_tag ml">本周</view>
+        </view>
+        <u-row>
+          <u-col :span="5">
+            <u-input v-model="filterForm.dateTime" @focus="hideOpenPicker('time')" placeholder="2022-00-00" border="none"></u-input>
+          </u-col>
+          <u-col :span="1">
+            <text style="color:#d1d3d9">-</text>
+          </u-col>
+          <u-col :span="5">
+            <u-input v-model="filterForm.dateTime" @focus="hideOpenPicker('time')" placeholder="结束时间" border="none"></u-input>
+          </u-col>
+        </u-row>
+      </view>
+      <view class="search_item">
+        <view class="title">
+          <text class="lable">选择分类</text>
+          <view class="time_tag">全选</view>
+        </view>
+        <view class="cate_list">
+          <u-tag text="标签" type="success" plain plainFill></u-tag>
+        </view>
+      </view>
     </view>
     <view class="footer">
       <u-row>
@@ -21,7 +39,8 @@
         </u-col>
       </u-row>
     </view>
-    <u-datetime-picker mode="date" :show="showDateTime" v-model="selectTime" :closeOnClickOverlay="true" @close="showDateTime = false" @cancel="showDateTime = false" @confirm="handleConfirmTime($event, item)">
+    <u-datetime-picker mode="date" :show="showDateTime" v-model="selectTime" :closeOnClickOverlay="true" @close="showDateTime = false"
+      @cancel="showDateTime = false" @confirm="handleConfirmTime($event, item)">
       aa
     </u-datetime-picker>
   </view>
@@ -35,7 +54,7 @@ export default {
       filterForm: {
         dateTime: '',
       },
-      filterFormRules:{},
+      filterFormRules: {},
     }
   },
   computed: {
@@ -68,7 +87,7 @@ export default {
 .popup_main {
   height: 100vh;
   width: 100%;
-  padding: 24rpx;
+  padding: 36rpx;
   position: relative;
   box-sizing: border-box;
   .footer {
@@ -79,22 +98,28 @@ export default {
     padding-bottom: env(safe-area-inset-bottom);
   }
 }
-.form_item {
-  display: flex;
-  justify-content: space-between;
-  .U-input {
-    flex: 1;
+.search_item {
+  border-bottom: 1px solid #d1d3d9;
+  .title {
+    margin: 24rpx 48rpx 24rpx 0;
+    display: flex;
+    align-items: center;
+    .lable {
+      margin: 0 24rpx 0 0;
+    }
+    .time_tag {
+      width: 88rpx;
+      text-align: center;
+      border: 1px solid #56aaff;
+      border-radius: 40rpx;
+      font-size: 24rpx;
+      height: 40rpx;
+    }
+    .ml {
+      margin-left: 24rpx;
+    }
   }
-  .time_tag {
-    width: 88rpx;
-    text-align: center;
-    border: 1px solid #56aaff;
-    border-radius: 40rpx;
-    font-size: 24rpx;
-    height: 40rpx;
-  }
-  .ml {
-    margin-left: 8rpx;
+  .cate_list {
   }
 }
 </style>
