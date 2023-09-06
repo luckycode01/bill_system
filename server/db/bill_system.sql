03088023e59106d7419b83fda5ac9682fe670798
13451167279 <1748595804@qq.com>
2023-09-06 23:33:45 +0800

    修改表结构

--- server/db/bill_system.sql
+++ server/db/bill_system.sql
@@ -17,24 +17,28 @@ use bill_system;
 /*==============================================================*/
 /* Table: user_manager                                          */
 /*==============================================================*/
-DROP TABLE IF EXISTS `user_manager`;
-create table user_manager (
-   `mg_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-   `mg_name` varchar(32) NOT NULL COMMENT '名称',
-   `mg_pwd` char(64) NOT NULL COMMENT '密码',
-   `role_ids` varchar(512) NOT NULL DEFAULT '' COMMENT '角色id',
-   `mg_mobile` varchar(32) DEFAULT NULL,
-   `mg_email` varchar(64) DEFAULT NULL,
-   `avator` varchar(100) NULL DEFAULT NULL comment '用户头像',
+DROP TABLE IF EXISTS `user_center`;
+create table user_center (
+   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+   `username` varchar(32) NOT NULL COMMENT '名称',
+   `password` char(64) NOT NULL COMMENT '密码',
+   `role_ids` varchar(512) NOT NULL DEFAULT '' COMMENT '角色id(逗号分割的字符串)',
+   `user_mobile` varchar(32) DEFAULT NULL COMMENT '手机号',
+   `user_email` varchar(64) DEFAULT NULL COMMENT '邮箱',
    `mg_state` tinyint(2) DEFAULT '1' COMMENT '1：表示启用 0:表示禁用',
    `create_time` int(10) unsigned NOT NULL COMMENT '创建时间',
    `update_time` int(10) unsigned NOT NULL COMMENT '修改时间',
    `user_type` int(2) NOT NULL DEFAULT '1' COMMENT '用户类型(1:管理用户，2:普通用户)',
    `deleted` tinyint NOT NULL COMMENT '逻辑删除(0未删除；1删除)',
-   PRIMARY KEY (mg_id)
+   `avator` varchar(100) NULL DEFAULT NULL comment '用户头像',
+   `user_sex` enum( '0', '1') NULL DEFAULT null COMMENT '性别（0：男，1：女）',
+   `user_edu` enum('10', '20', '30', '40', '50', '60', '70')  NULL DEFAULT null COMMENT '学历(70:博士, 60:硕士, 50:本科, 40:专科, 30:高中, 20:初中, 10:小学)',
+   `user_edustr` enum('博士', '硕士', '本科', '专科', '高中', '初中', '小学') NULL DEFAULT null COMMENT '学历(70:博士, 60:硕士, 50:本科, 40:专科, 30:高中, 20:初中, 10:小学)',
+   `user_introduce` text COMMENT '简介',
+   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=510 DEFAULT CHARSET=utf8 COMMENT='管理员表';
 
-INSERT INTO `user_manager` VALUES ('1', 'admin', '$2y$10$sZlpZNoLAnoD1DtYO9REAODCPkpMb5bwl4oMzrMvJa83k9BY3KRwq', '0,1,2', '12345678', 'adsfad@qq.com', null, '1', '1486720211',  '1486720211', '1', '0');
+INSERT INTO `user_center` VALUES ('1', 'admin', '$2y$10$sZlpZNoLAnoD1DtYO9REAODCPkpMb5bwl4oMzrMvJa83k9BY3KRwq', '0,1,2', '13412344321', 'adsfad@qq.com', '1', '1486720211',  '1486720211', '1', '0',null,null,null,null,null);
 
 -- ----------------------------
 -- Table structure for sp_role
@@ -138,33 +142,6 @@ INSERT INTO `sp_permission_api` VALUES ('5', '105', 'GoodService', 'createGood',
 INSERT INTO `sp_permission_api` VALUES ('6', '107', 'OrderService', 'getAllOrders', 'orders', null);
 INSERT INTO `sp_permission_api` VALUES ('9', '109', 'OrderService', 'createOrder', 'orders', null);
 INSERT INTO `sp_permission_api` VALUES ('10', '110', 'ManagerService', 'getAllManagers', 'users', null);
-
--- ----------------------------
--- Table structure for sp_user
--- ----------------------------
-CREATE TABLE `sp_user` (
-   `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-   `username` varchar(128) NOT NULL DEFAULT '' COMMENT '登录名',
-   `password` char(64) NOT NULL DEFAULT '' COMMENT '登录密码',
-   `user_email` varchar(64) NOT NULL DEFAULT '' COMMENT '邮箱',
-   `user_sex` enum('2', '0', '1') NOT NULL DEFAULT '2' COMMENT '性别（0：男，1：女）',
-   `user_mobile` varchar(32) NOT NULL DEFAULT '' COMMENT '手机',
-   `user_edu` enum('10', '20', '30', '40', '50', '60', '70')  NULL DEFAULT null COMMENT '学历(70:博士, 60:硕士, 50:本科, 40:专科, 30:高中, 20:初中, 10:小学)',
-   `user_edustr` enum('博士', '硕士', '本科', '专科', '高中', '初中', '小学') NULL DEFAULT null COMMENT '学历(70:博士, 60:硕士, 50:本科, 40:专科, 30:高中, 20:初中, 10:小学)',
-   `user_introduce` text COMMENT '简介',
-   `user_type` int(2) NOT NULL DEFAULT '2' COMMENT '用户类型(1:管理用户，2:普通用户)',
-   `mg_state` tinyint(2) DEFAULT '1' COMMENT '1：表示启用 0:表示禁用',
-   `deleted` tinyint NOT NULL COMMENT '逻辑删除(0未删除；1删除)',
-   `role_id` VARCHAR(512) NOT NULL DEFAULT '' COMMENT '角色id',
-   `avator` varchar(100) NULL DEFAULT NULL COMMENT '用户头像',
-   `create_time` int(11) NOT NULL COMMENT '创建时间',
-   `update_time` int(11) NOT NULL COMMENT '修改时间',
-   
-   PRIMARY KEY (`user_id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8 COMMENT = '用户表';
-
-INSERT INTO `sp_user` VALUES ('1', 'zce', '$2a$08$lV0Gr4AKx7xH7cCU4KCGCOikNzGPaWIpw9W7A9BONIxoJ2.hGC9qi', 'w@zce.me', '2', '13412344321', '60', '硕士', '简介简介简介','2', '1', '0', '0,1', null, '1512033129', '1512033129');
-INSERT INTO `sp_user` VALUES ('11', 'ww', '$2a$08$09nUxs.9czzXc4JZJTOdteeXSd/mxZVg96AhqciGbTMB6cfbGUWC2', 'i@zce.me', '2', '13467899876', '50', '本科', '简介1231233','2', '1', '0', '0,1', null,'1512122098', '1512033129');
 
 
 /*==============================================================*/
