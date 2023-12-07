bc5b116168c775d714c56634cf96d2d9d783f5ad
maws1008 <1748595804@qq.com>
2023-12-07 18:40:58 +0800

    登录调整

--- ruoyi-ui/src/views/login/components/LoginForm.vue
+++ ruoyi-ui/src/views/login/components/LoginForm.vue
@@ -1,46 +1,35 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">后台管理系统</h3>
+    
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules">
       <el-form-item prop="username">
         <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
           <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="loginForm.password" type="password" auto-complete="off" placeholder="密码" @keyup.enter.native="handleLogin">
+        <el-input v-model="loginForm.password" type="password" auto-complete="off" show-password placeholder="密码">
           <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
         </el-input>
       </el-form-item>
-      <!-- <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input
-          v-model="loginForm.code"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter.native="handleLogin"
-        >
+      <el-form-item prop="code" v-if="captchaEnabled">
+        <el-input v-model="loginForm.code" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter.native="handleLogin">
           <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
         </el-input>
         <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+          <img :src="codeUrl" @click="getCode" class="login-code-img" />
         </div>
-      </el-form-item> -->
+      </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+      <el-button type="text" size="mini" class="forget">忘记密码</el-button>
       <el-form-item style="width:100%;">
-        <el-button :loading="loading" size="medium" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
+        <el-button :loading="loading" class="login-btn" size="mini" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
-        <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
-        </div>
       </el-form-item>
     </el-form>
-    <!--  底部  -->
-    <div class="el-login-footer">
-      <span>Copyright © lucky.</span>
-    </div>
+    
   </div>
 </template>
 
@@ -138,36 +127,38 @@ export default {
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
-.login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
-  background-size: cover;
+<style rel="stylesheet/scss" lang="scss" scoped>
+.el-divider__text {
+  color: #707070;
 }
-.title {
+.login {
+  height: 100%;
+}
+.form-name {
   margin: 0px auto 30px auto;
   text-align: center;
   color: #707070;
 }
 
-.login-form {
-  border-radius: 6px;
-  background: #ffffff;
+
+.el-form {
   width: 400px;
-  padding: 25px 25px 5px 25px;
-  .el-input {
-    height: 38px;
+  ::v-deep .el-input {
+    height: 40px;
     input {
-      height: 38px;
+      height: 40px;
     }
   }
+  .login-btn {
+    height: 40px;
+    background: #0960bd;
+  }
   .input-icon {
-    height: 39px;
-    width: 14px;
+    font-size: 16px;
     margin-left: 2px;
+  }
+  .forget {
+    float: right;
   }
 }
 .login-tip {
---
+++ ruoyi-ui/src/views/login/index.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="login-main">
+        <div class="login-hidden">
+          <div class="logo anticon">
+            <img src="@/assets/logo/avatar.jpg" />
+            <div class="">
+              Lucky 服务平台
+            </div>
+          </div>
+          <div class="login-auto">
+            <img :alt="title" src="@/assets/images/svg/login-box-bg.svg" class="w-1/2 -mt-16 -enter-x" />
+            <div class="title">
+              <span> {{title }}</span>
+            </div>
+            <div class="desc">
+              输入您的个人详细信息开始使用！
+            </div>
+          </div>
+        </div>
+        <div class="login-form">
+          <div class="form-main">
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+              <el-tab-pane label="密码登录" name="password">
+                <LoginForm />
+              </el-tab-pane>
+              <el-tab-pane label="验证码登录" name="code">
+                
+              </el-tab-pane>
+            </el-tabs>
+            <ForgetPasswordForm />
+            <RegisterForm />
+            <MobileForm />
+            <QrCodeForm />
+            <el-divider>其他登录方式</el-divider>
+            <div class="othen-login flex space-around">
+              <img src="@/assets/images/png/weixin.png" alt="">
+              <img src="@/assets/images/png/qq.png" alt="">
+              <img src="@/assets/images/png/github.png" alt="">
+              <img src="@/assets/images/png/google.png" alt="">
+              <img src="@/assets/images/png/Twitter.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from "./components/LoginForm";
+export default {
+  components: { LoginForm },
+  data() {
+    return {
+      activeName:'password',
+      title: process.env.VUE_APP_TITLE,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  box-sizing: border-box;
+}
+::v-deep .el-tabs__nav-wrap::after{
+  display: none;
+}
+.login {
+  &-container {
+    width: 100%;
+    height: 100%;
+    padding: 0 16px;
+    overflow: hidden;
+    position: relative;
+    &::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      margin-left: -48%;
+      background: url("~@/assets/images/svg/login-bg.svg") no-repeat 100%;
+      background-size: auto 100%;
+      content: "";
+    }
+  }
+  &-box {
+    max-width: 1200px;
+    height: 100%;
+    padding: 30px 40px 0;
+    margin: auto;
+    .login-main {
+      height: 100%;
+      position: relative;
+      display: flex;
+      .othen-login {
+        img {
+          width: 28px;
+          height: 28px;
+          cursor: pointer;
+        }
+      }
+      ::v-deep .el-tabs__content{
+        min-width: 400px;
+        min-height: 272px;
+      }
+      ::v-deep .el-tabs__item{
+        font-size: 20px;
+        font-weight: 700;
+      }
+    }
+    .login-hidden {
+      width: 50%;
+      margin-right: 1rem;
+      padding-left: 16px;
+      display: flex;
+      .anticon {
+        text-align: center;
+        text-transform: none;
+        margin-left: 8px;
+        font-size: 24px;
+        color: #fff;
+        font-weight: 700;
+        transition: all 0.5s;
+        line-height: normal;
+        cursor: pointer;
+      }
+      .logo {
+        display: flex;
+        width: 50%;
+        height: 80px;
+        position: absolute;
+        top: 8px;
+        align-items: center;
+        img {
+          width: 48px;
+          height: 48px;
+          margin-right: 8px;
+        }
+      }
+      .login-auto {
+        margin: auto;
+        color: #fff;
+        img {
+          width: 50%;
+          height: auto;
+          margin-top: -4rem;
+        }
+        .title {
+          font-size: 1.875rem;
+          line-height: 2.25rem;
+          margin-top: 2.5rem;
+          font-weight: 500;
+          span {
+            margin-top: 1rem;
+          }
+        }
+        .desc {
+          font-size: 14px;
+          margin-top: 1.25rem;
+          font-weight: 400;
+        }
+      }
+    }
+    .login-form {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      .form-main {
+        padding: 1rem;
+        margin: auto auto auto 4rem;
+      }
+    }
+  }
+}
+</style>
