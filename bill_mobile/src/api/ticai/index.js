8f8a74c030ccfcfc67b93193064ff2026a8b5bdc
luckycode01 <61460857+luckycode01@users.noreply.github.com>
2023-12-21 11:22:11 +0800

    Delete bill_mobile directory

--- bill_mobile/src/api/ticai/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/api/request';
-export default {
-  reqGetWinNumList() {
-    return request({
-      url: `/ticai/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=1`,
-      method: 'get',
-    });
-  },
-};
